e160cad393d436c84788e4109c0ca4cddd52c247
sergej gricel <siarhei_hrytsel@icloud.com>
2022-12-03 22:17:03 +0300

    Added task0

---
+++ task0/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число: ");
+// String answer = Console.ReadLine(); //Сдесь вводиться строка данных. Например:  "528"
+// int number = Convert.ToInt32(answer); //Сдесь конвертируется строка данных."528" в значения 5, 2 и 8 
+int number = Convert.ToInt32(Console.ReadLine()); //вместо двух комманд (2 и 3) можно воспользоваться одной(4)
+Console.WriteLine(number * number);
