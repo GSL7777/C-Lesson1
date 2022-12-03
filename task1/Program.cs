43757f15c7f04d12c564a80b0324ebb5e2de039c
sergej gricel <siarhei_hrytsel@icloud.com>
2022-12-03 23:20:53 +0300

    Added task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+int number_3 = number_2 * number_2;
+//Console.WriteLine(number_3);
+if (number_1 == number_3)
+{
+     Console.WriteLine("Yes");   
+}
+else
+{
+     Console.WriteLine("No");
+}
