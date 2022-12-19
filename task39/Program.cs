82238755ea2249d3c8c9e6cc2b7a48c3b50e8cde
sergej gricel <siarhei_hrytsel@icloud.com>
2022-12-19 20:21:34 +0300

    Aded task39

---
+++ task39/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 38: 
+// Задайте массив натуральных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+int [] array = new int [5];
+for (int i = 0; i < array.Length; i++)
+{
+    array [i] = new Random().Next(1, 100);   
+}
+int max = array [0];
+for (int j = 0; j < array.Length; j++)
+{
+    if (array [j] > max)
+    {
+     max = array[j];   
+    }
+}
+int min = array [0];
+for (int j = 0; j < array.Length; j++)
+{
+    if (array [j] < min)
+    {
+     min = array[j];   
+    }
+}
+int diff = max - min;
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива:\n [{string.Join(", ", array)}] \nсоставляет: {diff}");
+
