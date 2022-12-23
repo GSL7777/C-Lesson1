83a0146a302f92e23dbf2878e8eb02cf59c6a0fa
sergej gricel <siarhei_hrytsel@icloud.com>
2022-12-23 20:36:25 +0300

    Added task31

---
+++ task31/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// Данные вводятся с консоли пользователем
+
+int [] array = new int [8];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите число массива:");
+    array [i] = Convert.ToInt32(Console.ReadLine());   
+}
+Console.WriteLine($"Массив введенных данных:[{string.Join (", ", array)}]");
