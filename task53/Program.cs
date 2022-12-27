33b9c068a833a72760b898e9f294bcef4b42e491
sergej gricel <siarhei_hrytsel@icloud.com>
2022-12-27 19:39:22 +0300

    Adedd task53

---
+++ task53/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// ​
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadInt (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int rightRange = 9)
+{
+    int[,] matr = new int[rows, columns];
+    var rand = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rand.Next(leftRange, rightRange + 1);   
+        }   
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.WriteLine(); 
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }  
+    }
+}
+
+void GetSumColumns(int[,] matr)
+{
+    for (int j = 0; j < matr.GetLength(1); j++) 
+    {
+        double summ = 0;
+        for (int i = 0; i < matr.GetLength(0);i++)     
+        {
+            summ += matr[i,j];
+        }
+        double average = summ/matr.GetLength(0);
+        int n = j + 1;
+        Console.WriteLine("Среднее значение " +n+" столбца: " + average);
+    
+    } 
+}
+
+int rowsCount = ReadInt("Введите число строк:");
+int columnsCount = ReadInt("Введите число столбцов:");
+int [,] matrix = GetRandomMatrix(rowsCount, columnsCount);
+PrintMatrix(matrix);
+Console.WriteLine();
+GetSumColumns(matrix);
+     
---
+++ task55/Program.cs
@@ -0,0 +1,85 @@
+﻿int ReadInt (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int rightRange = 9)
+{
+    int[,] matr = new int[rows, columns];
+    var rand = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rand.Next(leftRange, rightRange + 1);   
+        }   
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.WriteLine(); 
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }  
+    }
+}
+
+int[] GetSumColumns(int[,] matr)
+{
+//   int[] summ = new int [matr.GetLength(1)];
+    for (int j = 0, k = 0; j < matr.GetLength(1); j++) 
+    {
+        int summa = 0;
+        for (int i = 0; i < matr.GetLength(0);i++)     
+            {
+                summa += matr[i,j];
+            }
+                summ[k] = summa/matr.GetLength(1);
+                Console.WriteLine("Среднее арифметическое столбца "+(j+1)+" равно: "+summ[k]);
+                k++;
+    } 
+}
+
+     
+
+
+
+// {
+//     int[] summ = new int [matr.GetLength(1)];
+//     for (int j = 0; j < matr.GetLength(1); j++)
+//     {
+//         for (int i = 0; i < matr.GetLength(0); i++)
+//         {
+//             summ[j] += matr[i, j];
+//         }
+//     return summ;
+//     }
+// } 
+
+
+
+// {
+//     int sum = 0;
+//     int j = 0;
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//         {
+//             sum += matr[i, j];
+//         }
+//     int aver = sum/matr.GetLength(0);
+//     return aver;
+// }      
+
+
+int rowsCount = ReadInt("Введите число строк:");
+int columnsCount = ReadInt("Введите число столбцов:");
+int [,] matrix = GetRandomMatrix(rowsCount, columnsCount);
+PrintMatrix(matrix);
+int[] summ = GetSumColumns(matrix);
+Console.WriteLine();
+Console.WriteLine(summ);
