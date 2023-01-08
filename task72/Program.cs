b116c76ee90c260f7dc037ec99280cbb32202cb6
sergej gricel <siarhei_hrytsel@icloud.com>
2023-01-08 20:44:50 +0300

    Added task72

---
+++ task72/Program.cs
@@ -0,0 +1,20 @@
+﻿// Домашняя работа
+// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите начальное число диапазона чисел:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное число диапазона чисел:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumNumb (int min, int max)
+{
+if (min > max) return -1;
+if (min == max) return max;
+min = min + SumNumb (min+1, max); 
+return min; 
+}
+Console.WriteLine();
+Console.WriteLine(SumNumb (M,N));
