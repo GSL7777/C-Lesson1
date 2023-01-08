db5974273e2a2cc2ddf5c26e6e2eb60ecc51317f
sergej gricel <siarhei_hrytsel@icloud.com>
2023-01-08 20:44:40 +0300

    Added task71

---
+++ task71/Program.cs
@@ -0,0 +1,17 @@
+﻿// Домашняя работа
+// Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int min = 1;
+
+int LineNumbers(int min, int max)
+{
+    if(max <= 0) return -1;
+    if(min < max) 
+    Console.Write(LineNumbers(min+1, max));
+    return min;   
+}
+Console.WriteLine (LineNumbers(min, N));
