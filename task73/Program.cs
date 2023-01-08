c88b5ab189b20becd64aefa5055beca7bc9654a2
sergej gricel <siarhei_hrytsel@icloud.com>
2023-01-08 20:44:57 +0300

    Added task73

---
+++ task73/Program.cs
@@ -0,0 +1,18 @@
+﻿// Домашняя работа
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Ackerman (int m, int n)
+{
+if (m == 0) return n + 1;
+if (n == 0) return Ackerman (m-1, 1);
+else return Ackerman (m-1, Ackerman (m, n - 1));
+}
+Console.WriteLine();
+Console.WriteLine(Ackerman (m, n));
